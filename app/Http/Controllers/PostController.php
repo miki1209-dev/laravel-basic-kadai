c1a5cf0b34a2c31c9192b1485b6c81b06b3aeda6
miki1209-dev <crest0421@gmail.com>
2025-03-30 17:04:08 +0900

    データの作成機能とバリデーションを実装しよう

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\SUpport\Facades\DB;
 use App\Models\Post;
+use App\Http\Requests\PostStoreRequest;
 
 class PostController extends Controller
 {
@@ -19,5 +20,25 @@ class PostController extends Controller
 		$post = Post::find($id);
 
 		return view('posts.show', compact('post'));
+	}
+
+	public function create()
+	{
+		return view('posts.create');
+	}
+
+	public function store(PostStoreRequest $request)
+	{
+		$post = new Post();
+		$post->title = $request->input('title');
+		$post->content = $request->input('content');
+		$post->save();
+
+		//デバック用
+		// dump($post->id);
+		// dump($post->title);
+		// dump($post->content);
+
+		return redirect('/posts');
 	}
 }
